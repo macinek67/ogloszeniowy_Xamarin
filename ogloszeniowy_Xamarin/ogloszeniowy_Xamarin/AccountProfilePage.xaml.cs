774228ed5c8eb6e6806b7f883169ddd3a4c8c2e2
macinek67 <marcindzik1234@gmail.com>
2023-12-24 15:22:40 +0100

    accountProfilePage back-end finished

--- ogloszeniowy_Xamarin/ogloszeniowy_Xamarin/AccountProfilePage.xaml.cs
+++ ogloszeniowy_Xamarin/ogloszeniowy_Xamarin/AccountProfilePage.xaml.cs
@@ -13,10 +13,59 @@ namespace ogloszeniowy_Xamarin
 	public partial class AccountProfilePage : ContentPage
 	{
 		User user;
+		User_data userData;
 		public AccountProfilePage (User user)
 		{
 			InitializeComponent ();
 			this.user = user;
-		}
-	}
+			UploadData();
+        }
+
+		public async void UploadData()
+		{
+            var temp = (await App.Database.GetUser_data(user.User_id));
+            if (temp == null || temp.Count() == 0)
+            {
+                User_data newUserData = new User_data()
+                {
+                    User_id = user.User_id
+                };
+                await App.Database.InsertUser_data(newUserData);
+            }
+            userData = temp[0];
+
+			if(userData.Name != null) name_Entry.Text = userData.Name;
+			if(userData.Surname != null) surname_Entry.Text = userData.Surname;
+			if(userData.Currnent_occupation != null) currentOccupation_entry.Text = userData.Currnent_occupation;
+			if(userData.City != null) city_Entry.Text = userData.City;
+			if(userData.Birth_date != null) birthDate_Entry.Date = userData.Birth_date;
+			if(userData.Telephone_number != null) telephone_Entry.Text = userData.Telephone_number;
+			if(userData.Summary != null) occupationSummary_Editor.Text = userData.Summary;
+        }
+
+        private async void personalDataSaveButton_Clicked(object sender, EventArgs e)
+        {
+            userData.Name = name_Entry.Text;
+            userData.Surname = surname_Entry.Text;
+            userData.Currnent_occupation = currentOccupation_entry.Text;
+            userData.City = city_Entry.Text;
+
+            await App.Database.UpdateUser_data(userData);
+        }
+
+        private async void contactDataSaveButton_Clicked(object sender, EventArgs e)
+        {
+            userData.Birth_date = birthDate_Entry.Date;
+            userData.Telephone_number = telephone_Entry.Text;
+
+            await App.Database.UpdateUser_data(userData);
+        }
+
+        private async void occupationSummarySaveButton_Clicked(object sender, EventArgs e)
+        {
+            userData.Summary = occupationSummary_Editor.Text;
+
+            await App.Database.UpdateUser_data(userData);
+        }
+    }
 }
