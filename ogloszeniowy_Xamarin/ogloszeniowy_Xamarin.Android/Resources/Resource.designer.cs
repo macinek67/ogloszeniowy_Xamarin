1036b7151837543b6fdd5eb9a578fc72f5c6d133
macinek67 <marcindzik1234@gmail.com>
2023-12-30 19:53:58 +0100

    main page finished

--- ogloszeniowy_Xamarin/ogloszeniowy_Xamarin.Android/Resources/Resource.designer.cs
+++ ogloszeniowy_Xamarin/ogloszeniowy_Xamarin.Android/Resources/Resource.designer.cs
@@ -16678,6 +16678,9 @@ namespace ogloszeniowy_Xamarin.Droid
 			// aapt resource value: 0x7F070094
 			public const int tooltip_frame_light = 2131165332;
 			
+			// aapt resource value: 0x7F070095
+			public const int xamBaner = 2131165333;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
